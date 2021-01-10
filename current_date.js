a3f38589ae13028844574c12cf0d5b4e6061f5d2
braulio0790 <braulio90.lds@gmail.com>
2021-01-09 22:16:08 -0600

    init comment

--- current_date.js
+++ current_date.js
@@ -1,3 +1,15 @@
 console.log("hello world");
 
+function getYear(){
+  var d = new Date();
+  var n = d.getFullYear();
+  document.getElementById("yearC").innerHTML = n;
 
+
+  try {
+    const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+    document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', options);
+  } catch (e) {
+    alert('Error with code or your browser does not support Locale');
+  }
+}
