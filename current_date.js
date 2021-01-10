84161230c6e6f2940f4fc3accd483ce4e6e8ba5c
braulio0790 <braulio90.lds@gmail.com>
2021-01-09 20:35:04 -0600

    init commit

--- current_date.js
+++ current_date.js
@@ -5,3 +5,9 @@ var today = new Date();
   var date = today.getFullYear()+'-'+(today.getMonth()+1)+' '+today.getDate();
 
   document.getElementById("currentDate").innerHTML = date;
+
+// Date object 2
+var string = document.lastModified;
+
+//alert
+alert(document.lastModified);
