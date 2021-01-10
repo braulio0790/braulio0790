59ccd0a2ef593a77b1d1ce3bf7da10b13fa4b36f
braulio0790 <braulio90.lds@gmail.com>
2021-01-09 22:45:30 -0600

    init commit

--- current_date.js
+++ current_date.js
@@ -5,11 +5,7 @@ function getYear(){
   var n = d.getFullYear();
   document.getElementById("yearC").innerHTML = n;
 
+    var x = document.lastModified;
+    document.getElementById("currentHour").innerHTML = x;
 
-  try {
-    const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-    document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', options);
-  } catch (e) {
-    alert('Error with code or your browser does not support Locale');
-  }
 }
