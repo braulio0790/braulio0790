5be7ee7ff7fba12d9d5ba8cd2472bcf0253ac3a4
braulio0790 <braulio90.lds@gmail.com>
2021-01-23 14:34:09 -0600

    init commit

---
+++ lesson3/js/current_date.js
@@ -0,0 +1,11 @@
+console.log("hello world");
+
+function getYear(){
+  var d = new Date();
+  var n = d.getFullYear();
+  document.getElementById("yearC").innerHTML = n;
+
+    var x = document.lastModified;
+    document.getElementById("currentHour").innerHTML = x;
+
+}
